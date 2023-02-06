e88b4c52b77b6f0550814e540dbc03640abe6add
FantomeBeignet <himself@fantomebeig.net>
2023-02-06 17:59:31 +0100

    refactor: switch to use redis list

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,7 @@ import redis, { databaseName } from '$lib/redis';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-	const data = await redis.hgetall(databaseName);
+	const data = await redis.llen('delays');
 	if (!data) return { lateCounter: 0 };
-	return { lateCounter: Object.keys(data).length };
+	return { lateCounter: data };
 }) satisfies PageServerLoad; 
