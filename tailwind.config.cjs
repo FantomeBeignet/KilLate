e52a78d0b794fea8a2cd9c11302b18f7862738ed
FantomeBeignet <himself@fantomebeig.net>
2023-02-06 15:16:55 +0100

    chore: color alias in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: colors.purple,
+				neutral: colors.slate
+			}
+		}
+	},
+	plugins: []
+};
