6b6b22e551652bd9089ee0f334f7df7b6630de4d
FantomeBeignet <himself@fantomebeig.net>
2023-02-07 16:57:23 +0100

    feat: pusher config

---
+++ src/lib/pusher/client.ts
@@ -0,0 +1,8 @@
+import Pusher from 'pusher-js';
+import { PUBLIC_PUSHER_KEY, PUBLIC_PUSHER_CLUSTER } from '$env/static/public';
+
+const pusher = new Pusher(PUBLIC_PUSHER_KEY, {
+    cluster: PUBLIC_PUSHER_CLUSTER
+});
+
+export default pusher;
