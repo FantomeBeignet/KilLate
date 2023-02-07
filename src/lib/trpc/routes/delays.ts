afbdfb25e642887752292ca22a6ccbcf9700cceb
FantomeBeignet <himself@fantomebeig.net>
2023-02-07 17:43:44 +0100

    refactor: test to see if it works better with vercel

--- src/lib/trpc/routes/delays.ts
+++ src/lib/trpc/routes/delays.ts
@@ -1,8 +1,17 @@
 import redis from '$lib/redis';
 import { t } from '$lib/trpc/t';
 import { z } from 'zod';
-import pusher from '$lib/pusher/server';
+import { PUSHER_APP_ID, PUSHER_SECRET } from '$env/static/private';
+import { PUBLIC_PUSHER_CLUSTER, PUBLIC_PUSHER_KEY } from '$env/static/public';
+import Pusher from 'pusher';
 
+const pusher = new Pusher({
+	appId: PUSHER_APP_ID,
+	key: PUBLIC_PUSHER_KEY,
+	secret: PUSHER_SECRET,
+	cluster: PUBLIC_PUSHER_CLUSTER,
+	useTLS: true
+});
 export const delays = t.router({
 	add: t.procedure.input(z.string()).mutation(async ({ input }) => {
 		redis.lpush('delays', input);
