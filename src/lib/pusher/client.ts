bfa492374319d52e2e6a11df2b6853afd2efa02d
FantomeBeignet <himself@fantomebeig.net>
2023-02-08 17:36:20 +0100

    style: formatting

--- src/lib/pusher/client.ts
+++ src/lib/pusher/client.ts
@@ -2,7 +2,7 @@ import Pusher from 'pusher-js';
 import { PUBLIC_PUSHER_KEY, PUBLIC_PUSHER_CLUSTER } from '$env/static/public';
 
 const pusher = new Pusher(PUBLIC_PUSHER_KEY, {
-    cluster: PUBLIC_PUSHER_CLUSTER
+	cluster: PUBLIC_PUSHER_CLUSTER
 });
 
 export default pusher;
